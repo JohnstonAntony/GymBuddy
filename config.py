ef4bcdf35a7562a3d0cd9db3f31974a6c2ffc06e
Johnston Antony <Johnston4u19@gmail.com>
2026-04-17 11:39:47 +0100

    Updated configpy to set up foundation for SQLalchemy aided database.

--- config.py
+++ config.py
@@ -3,23 +3,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+base_directory = os.path.abspath(os.path.dirname(__file__)) # absolute path to directory
+
 
 class Config:
     """Base configuration. Shared across all environments."""
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-secret-change-me"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # disabled to save memory and improve perf
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DATABASE_URL")
+        or f"sqlite:///{os.path.join(base_directory, 'instance', 'gymbuddy_dev.db')}" #file pathing for db inside instance folder
+    )
 
 
-class TestingConfig(Config):
+class TestingConfig(Config): # testing cofig to see if SQalchemy is working correctly
     TESTING = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" 
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 config_by_name = {
